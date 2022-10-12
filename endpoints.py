80e478873187d8faf82e5403c0fdccd6f41a951c
Artem <44943333+lifeavg@users.noreply.github.com>
2022-10-12 23:41:50 +0300

    separate redis loop

--- endpoints.py
+++ endpoints.py
@@ -1,21 +1,22 @@
 import asyncio
 import json
 from dataclasses import asdict
+from functools import partial
+from typing import Coroutine
 from uuid import UUID, uuid4
+
 from pydantic.json import pydantic_encoder
 from redis.asyncio.client import PubSub, Redis
 from starlette import status
+from starlette.authentication import requires
 from starlette.endpoints import WebSocketEndpoint
 from starlette.types import Receive, Scope, Send
 from starlette.websockets import WebSocket
-from starlette.authentication import requires
 
 from connections import redis as global_redis
-from schemas import Message, UserStatus, MessageStatus
+from schemas import Message, MessageStatus, UserStatus
 from utils import to_redis_key
 
-async def check_key(key: str, redis: Redis) -> tuple[str, str | None]:
-    return key, await redis.get(key)
 
 class ChatEndpoint(WebSocketEndpoint):
     encoding = 'json'
@@ -28,87 +29,57 @@ class ChatEndpoint(WebSocketEndpoint):
         super().__init__(scope, receive, send)
         self.redis: Redis = redis
         self.pubsub: PubSub = self.redis.pubsub()
+        self.pubsub_task: asyncio.Task | None = None
+
+    async def on_message(self, data: dict, websocket: WebSocket) -> None:
+        try:
+            json_data = json.loads(data['data'])
+            if not isinstance(json_data, dict):
+                # TODO exception or close with code ???
+                raise ValueError(
+                    f'Incorrect data dict in cache for message user={websocket.user.user}')
+        except json.JSONDecodeError as exception:
+            # TODO cache error
+            pass
+        else:
+            # TypeError: Message.__init__() missing 1 required positional argument:
+            message_data = Message(**json_data)
+            await websocket.send_json(asdict(message_data))
+
+    async def load_permissions(self, websocket: WebSocket) -> dict[str, Coroutine]:
+        subs = dict()
+        async for key in self.redis.scan_iter(f'permission:{websocket.user.user.uuid}'):
+            resource = key.split(':')
+            if resource[1] == 'chat':
+                subs[f'{resource[1]}:{resource[2]}'] = partial(
+                    self.on_message, websocket=websocket)
+        subs['chat:3a78e770-6789-4e4a-9286-73ee6cd283a6'] = partial(
+            self.on_message, websocket=websocket)
+        return subs
+
+    async def pubsub_listener(self, pubsub: PubSub) -> None:
+        async for data in pubsub.listen():  # type: ignore
+            print('pubsub', data)
 
     @requires('authenticated')
     async def on_connect(self, websocket: WebSocket) -> None:
         await websocket.accept()
-        # TODO init user permissions
-        await self.pubsub.subscribe('chat:3a78e770-6789-4e4a-9286-73ee6cd283a6')
+        # init user permissions
+        await self.pubsub.subscribe(f'permission:update:{websocket.user.user.uuid}')
+        await self.pubsub.subscribe(**await self.load_permissions(websocket))
         await self.redis.set(
             name=to_redis_key(websocket.user.user),
             value=json.dumps(UserStatus.ONLINE, default=pydantic_encoder))
+        self.pubsub_task = asyncio.create_task(
+            self.pubsub_listener(self.pubsub), name='pubsub_listener_task')
 
     async def on_receive(self, websocket: WebSocket, data: dict) -> None:
         await self._process_message(message=Message(**data), websocket=websocket)
 
     async def on_disconnect(self, websocket: WebSocket, close_code: int):
-        await self.redis.delete(to_redis_key(websocket.user.user)) 
-
-    async def dispatch(self) -> None:
-        websocket = WebSocket(
-            scope=self.scope, receive=self.receive, send=self.send)
-        await self.on_connect(websocket=websocket)
-        if websocket.user.is_authenticated:
-            close_code = status.WS_1000_NORMAL_CLOSURE
-            pending_tasks: set[asyncio.Task] = set()
-            pending_tasks.add(asyncio.create_task(
-                coro=self._dispatch_websocket(websocket=websocket), name='socket_connection'))
-            pending_tasks.add(asyncio.create_task(
-                coro=self._dispatch_pubsub(websocket=websocket, timeout=5), name='redis_subscription'))
-            try:
-                while True:
-                    done, pending_tasks = await asyncio.wait(
-                        fs=pending_tasks,
-                        return_when=asyncio.FIRST_COMPLETED)
-                    result = await self._process_dispatch_results(
-                        tasks=done,
-                        pending_tasks=pending_tasks,
-                        websocket=websocket)
-                    if result:
-                        close_code = result
-                        break
-            except Exception as exception:
-                close_code = status.WS_1011_INTERNAL_ERROR
-                raise exception
-            finally:
-                for task in pending_tasks:
-                    task.cancel()
-                await self.on_disconnect(websocket=websocket, close_code=close_code)
-
-    async def _dispatch_websocket(self, websocket: WebSocket) -> None | int:
-        message = await websocket.receive()
-        if message['type'] == 'websocket.receive':
-            data = await self.decode(websocket, message)
-            await self.on_receive(websocket=websocket, data=data)
-        elif message['type'] == 'websocket.disconnect':
-            return int(message.get("code") or status.WS_1000_NORMAL_CLOSURE)
-        else:
-            raise ValueError(
-                f'Unexpected value \'type \'={message["type"]} in websocket={websocket}')
-
-    async def _dispatch_pubsub(self, websocket: WebSocket, timeout: float = 0.0) -> None:
-        if self.pubsub.subscribed:
-            raw_data = await self.pubsub.get_message(ignore_subscribe_messages=True, timeout=timeout)
-            if raw_data:
-                await self._on_receive_pubsub(raw_data=raw_data['data'], websocket=websocket)
-        else:
-            await asyncio.sleep(timeout)
-
-    async def _on_receive_pubsub(self, raw_data: str, websocket: WebSocket) -> None:
-        if not raw_data:
-            return None # TODO missing cache
-        if not isinstance(raw_data, str):
-            raise ValueError(f'Incorrect data str in cache for message user={websocket.user.user}') # TODO exception or close with code ???
-        try:
-            json_data = json.loads(raw_data)
-            if not isinstance(json_data, dict):
-                raise ValueError(f'Incorrect data dict in cache for message user={websocket.user.user}') # TODO exception or close with code ???
-        except json.JSONDecodeError as exception:
-            # TODO cache error
-            pass
-        else:
-            message_data = Message(**json_data) # TypeError: Message.__init__() missing 1 required positional argument:
-            await websocket.send_json(asdict(message_data))
+        await self.redis.delete(to_redis_key(websocket.user.user))
+        if self.pubsub_task:
+            self.pubsub_task.cancel()
 
     async def _guard_chat_permitted(self, websocket: WebSocket, receiver: UUID) -> bool:
         # permission:user_uuid:resource_class_name:resource_uuid
@@ -116,12 +87,16 @@ class ChatEndpoint(WebSocketEndpoint):
 
     async def _sent_message(self, message: Message, websocket: WebSocket) -> None:
         if not message.receiver:
-            raise ValueError(f'Missing receiver in message from user={websocket.user.user}')
+            raise ValueError(
+                f'Missing receiver in message from user={websocket.user.user}')
         if not await self._guard_chat_permitted(websocket, message.receiver):
-            raise ValueError(f'Access denied for user={websocket.user.user} to {message.receiver}') # TODO exception or close with code ???
+            # TODO exception or close with code ???
+            raise ValueError(
+                f'Access denied for user={websocket.user.user} to {message.receiver}')
         message.sender = websocket.user.user
         message.uuid = uuid4()
-        no_text_message = Message(uuid=message.uuid, receiver=message.receiver, status=message.status)
+        no_text_message = Message(
+            uuid=message.uuid, receiver=message.receiver, status=message.status)
         await self.redis.set(
             name=to_redis_key(no_text_message),  # type: ignore
             value=json.dumps(no_text_message, default=pydantic_encoder))
@@ -131,25 +106,35 @@ class ChatEndpoint(WebSocketEndpoint):
 
     async def _update_message(self, message: Message, websocket: WebSocket) -> None:
         if not message.uuid:
-            raise ValueError(f'Missing uuid in update message from user={websocket.user.user}')
-        cached_data = await self.redis.get(name=to_redis_key(message))  # type: ignore
+            raise ValueError(
+                f'Missing uuid in update message from user={websocket.user.user}')
+        cached_data = await self.redis.get(name=to_redis_key(message)) # type: ignore
         if not cached_data:
-            return None # TODO missing cache
+            return None  # TODO missing cache
         if not isinstance(cached_data, str):
-            raise ValueError(f'Incorrect data in cache for message {message.uuid} user={websocket.user.user}') # TODO exception or close with code ???
+            # TODO exception or close with code ???
+            raise ValueError(
+                f'Incorrect data in cache for message {message.uuid} user={websocket.user.user}')
         try:
             json_data = json.loads(cached_data)
             if not isinstance(json_data, dict):
-                raise ValueError(f'Incorrect data in cache for message {message.uuid} user={websocket.user.user}') # TODO exception or close with code ???
+                # TODO exception or close with code ???
+                raise ValueError(
+                    f'Incorrect data in cache for message {message.uuid} user={websocket.user.user}')
         except json.JSONDecodeError as exception:
             # TODO cache error
             pass
         else:
-            cached_message = Message(**json_data) # TypeError: Message.__init__() missing 1 required positional argument:
+            # TypeError: Message.__init__() missing 1 required positional argument:
+            cached_message = Message(**json_data)
             if not (cached_message.receiver and cached_message.uuid):
-                raise ValueError(f'Incorrect data in cache for message {message.uuid} user={websocket.user.user}') # TODO exception or close with code ???
+                # TODO exception or close with code ???
+                raise ValueError(
+                    f'Incorrect data in cache for message {message.uuid} user={websocket.user.user}')
             if not await self._guard_chat_permitted(websocket, cached_message.receiver):
-                raise ValueError(f'Access denied for user={websocket.user.user} to {message.receiver}') # TODO exception or close with code ???
+                # TODO exception or close with code ???
+                raise ValueError(
+                    f'Access denied for user={websocket.user.user} to {message.receiver}')
             cached_message.status = message.status
             await self.redis.set(
                 name=to_redis_key(cached_message),  # type: ignore
@@ -166,28 +151,10 @@ class ChatEndpoint(WebSocketEndpoint):
                 await self._update_message(message=message, websocket=websocket)
             case MessageStatus.READ:
                 await self._update_message(message=message, websocket=websocket)
-            case _ :
+            case _:
                 await websocket.close(
-                    code=status.WS_1008_POLICY_VIOLATION, # TODO close codes
+                    code=status.WS_1008_POLICY_VIOLATION,  # TODO close codes
                     reason='Unexpected data received aaa')
-                raise ValueError(f'Incorrect MessageStatus user={websocket.user.user} message={message.uuid}, receiver={message.receiver}') # TODO exception or close with code ???
-
-    async def _process_dispatch_results(
-            self,
-            tasks: set[asyncio.Task],
-            pending_tasks: set[asyncio.Task],
-            websocket: WebSocket) -> int | None:
-        for task in tasks:
-            result = await task
-            task_name = task.get_name()
-            if task_name == 'socket_connection' and result:
-                return result
-            elif task_name == 'socket_connection' and not result:
-                pending_tasks.add(asyncio.create_task(
-                    coro=self._dispatch_websocket(websocket), name='socket_connection'))
-            elif task_name == 'redis_subscription':
-                pending_tasks.add(asyncio.create_task(
-                    coro=self._dispatch_pubsub(websocket, 5), name='redis_subscription'))
-            else:
+                # TODO exception or close with code ???
                 raise ValueError(
-                    f'Unexpected task result={result} task_name={task_name}')
+                    f'Incorrect MessageStatus user={websocket.user.user} message={message.uuid}, receiver={message.receiver}')
