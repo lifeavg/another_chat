2f43fbb6ad17864e730bdaeb6af541abffab5bcd
Artem <44943333+lifeavg@users.noreply.github.com>
2022-12-10 16:38:57 +0300

    adapt fixtures to models

--- auth/test/models_fixtures.py
+++ auth/test/models_fixtures.py
@@ -1,6 +1,6 @@
 import random
 import string
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from random import randint
 
 import pytest
@@ -9,9 +9,10 @@ from sqlalchemy import delete, select
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import selectinload, sessionmaker
 
-from ..auth.api.schemas import AccessAttemptResult, LoginAttemptResult
-from ..auth.db.models import (AccessAttempt, AccessSession, LoginAttempt,
-                              LoginSession, Permission, Service, User)
+from auth.api.schemas import AccessAttemptResult, LoginAttemptResult
+from auth.db.models import (AccessAttempt, AccessSession, LoginAttempt,
+                            LoginSession, Permission, Service, User)
+from auth.security import password_hash
 
 TEARDOWN: bool = False
 
@@ -46,9 +47,9 @@ async def session(engine):
 @pytest_asyncio.fixture
 async def active_user(session):
     user = User(
-        external_id=randint(0, 100),
         login=random_string(10),
-        password=random_string(10),
+        password=password_hash(random_string(10)),
+        confirmed=True,
         active=True)
     session.add(user)
     await session.commit()
@@ -64,10 +65,11 @@ async def active_user(session):
 
 @pytest_asyncio.fixture
 async def inactive_user(session):
-    user = User(external_id=randint(0, 100),
-                login=random_string(10),
-                password=random_string(10),
-                active=False)
+    user = User(
+        login=random_string(10),
+        password=password_hash(random_string(10)),
+        confirmed=True,
+        active=False)
     session.add(user)
     await session.commit()
     user = (await session.execute(select(User)
@@ -78,102 +80,6 @@ async def inactive_user(session):
     if TEARDOWN:
         await session.execute(delete(User)
                               .where(User.id == user.id))
-        await session.commit()
-
-
-@pytest_asyncio.fixture
-async def successful_login_attempt(session, active_user):
-    login_attempt = LoginAttempt(
-        user_id=active_user.id,
-        fingerprint=random_string(10),
-        date_time=datetime.utcnow(),
-        response=LoginAttemptResult.SUCCESS.value)
-    session.add(login_attempt)
-    await session.commit()
-    yield login_attempt, active_user
-    if TEARDOWN:
-        await session.execute(delete(LoginAttempt)
-                              .where(LoginAttempt.id == login_attempt.id))
-        await session.commit()
-
-
-@pytest_asyncio.fixture
-async def unsuccessful_login_attempt(session, active_user):
-    login_attempt = LoginAttempt(
-        user_id=active_user.id,
-        fingerprint=random_string(10),
-        date_time=datetime.utcnow(),
-        response=LoginAttemptResult.INCORRECT_PASSWORD.value)
-    session.add(login_attempt)
-    await session.commit()
-    yield login_attempt, active_user
-    if TEARDOWN:
-        await session.execute(delete(LoginAttempt)
-                              .where(LoginAttempt.id == login_attempt.id))
-        await session.commit()
-
-
-@pytest_asyncio.fixture
-async def active_login_session(session, successful_login_attempt):
-    login_session = LoginSession(
-        user_id=successful_login_attempt[1].id,
-        start=datetime.utcnow(),
-        end=datetime.utcnow() + timedelta(minutes=60),
-        stopped=False)
-    session.add(login_session)
-    await session.commit()
-    yield login_session
-    if TEARDOWN:
-        await session.execute(delete(LoginSession)
-                              .where(LoginSession.id == login_session.id))
-        await session.commit()
-
-
-@pytest_asyncio.fixture
-async def inactive_login_session(session, unsuccessful_login_attempt):
-    login_session = LoginSession(
-        user_id=unsuccessful_login_attempt[1].id,
-        start=datetime.utcnow() - timedelta(minutes=60),
-        end=datetime.utcnow(),
-        stopped=True)
-    session.add(login_session)
-    await session.commit()
-    yield login_session
-    if TEARDOWN:
-        await session.execute(delete(LoginSession)
-                              .where(LoginSession.id == login_session.id))
-        await session.commit()
-
-
-@pytest_asyncio.fixture
-async def successful_access_attempt(session, active_login_session):
-    access_attempt = AccessAttempt(
-        login_session_id=active_login_session.id,
-        fingerprint=random_string(10),
-        date_time=datetime.utcnow(),
-        response=AccessAttemptResult.SUCCESS.value)
-    session.add(access_attempt)
-    await session.commit()
-    yield access_attempt, active_login_session
-    if TEARDOWN:
-        await session.execute(delete(AccessAttempt)
-                              .where(AccessAttempt.id == access_attempt.id))
-        await session.commit()
-
-
-@pytest_asyncio.fixture
-async def active_access_session(session, successful_access_attempt):
-    access_session = AccessSession(
-        login_session_id=successful_access_attempt[1].id,
-        start=datetime.utcnow(),
-        end=datetime.utcnow() - timedelta(minutes=30),
-        stopped=False)
-    session.add(access_session)
-    await session.commit()
-    yield access_session
-    if TEARDOWN:
-        await session.execute(delete(AccessSession)
-                              .where(AccessSession.id == access_session.id))
         await session.commit()
 
 
@@ -217,4 +123,100 @@ async def user_permission(session, service_permission, active_user):
     yield user, service_permission
     if TEARDOWN:
         user.permissions.remove(service_permission)
+        await session.commit()
+
+
+@pytest_asyncio.fixture
+async def successful_login_attempt(session, user_permission):
+    login_attempt = LoginAttempt(
+        user_id=user_permission[0].id,
+        fingerprint=random_string(10),
+        date_time=datetime.now(timezone.utc),
+        response=LoginAttemptResult.SUCCESS.value)
+    session.add(login_attempt)
+    await session.commit()
+    yield login_attempt, user_permission
+    if TEARDOWN:
+        await session.execute(delete(LoginAttempt)
+                              .where(LoginAttempt.id == login_attempt.id))
+        await session.commit()
+
+
+@pytest_asyncio.fixture
+async def unsuccessful_login_attempt(session, user_permission):
+    login_attempt = LoginAttempt(
+        user_id=user_permission[0].id,
+        fingerprint=random_string(10),
+        date_time=datetime.now(timezone.utc),
+        response=LoginAttemptResult.INCORRECT_PASSWORD.value)
+    session.add(login_attempt)
+    await session.commit()
+    yield login_attempt, user_permission
+    if TEARDOWN:
+        await session.execute(delete(LoginAttempt)
+                              .where(LoginAttempt.id == login_attempt.id))
+        await session.commit()
+
+
+@pytest_asyncio.fixture
+async def active_login_session(session, successful_login_attempt):
+    login_session = LoginSession(
+        user_id=successful_login_attempt[1].id,
+        start=datetime.now(timezone.utc),
+        end=datetime.now(timezone.utc) + timedelta(minutes=60),
+        stopped=False)
+    session.add(login_session)
+    await session.commit()
+    yield login_session
+    if TEARDOWN:
+        await session.execute(delete(LoginSession)
+                              .where(LoginSession.id == login_session.id))
+        await session.commit()
+
+
+@pytest_asyncio.fixture
+async def inactive_login_session(session, unsuccessful_login_attempt):
+    login_session = LoginSession(
+        user_id=unsuccessful_login_attempt[1].id,
+        start=datetime.now(timezone.utc) - timedelta(minutes=120),
+        end=datetime.now(timezone.utc) - timedelta(minutes=20),
+        stopped=True)
+    session.add(login_session)
+    await session.commit()
+    yield login_session
+    if TEARDOWN:
+        await session.execute(delete(LoginSession)
+                              .where(LoginSession.id == login_session.id))
+        await session.commit()
+
+
+@pytest_asyncio.fixture
+async def successful_access_attempt(session, active_login_session):
+    access_attempt = AccessAttempt(
+        login_session_id=active_login_session.id,
+        fingerprint=random_string(10),
+        date_time=datetime.now(timezone.utc),
+        response=AccessAttemptResult.SUCCESS.value)
+    session.add(access_attempt)
+    await session.commit()
+    yield access_attempt, active_login_session
+    if TEARDOWN:
+        await session.execute(delete(AccessAttempt)
+                              .where(AccessAttempt.id == access_attempt.id))
+        await session.commit()
+
+
+@pytest_asyncio.fixture
+async def active_access_session(session, successful_access_attempt):
+    access_session = AccessSession(
+        login_session_id=successful_access_attempt[1].id,
+        start=datetime.now(timezone.utc),
+        end=datetime.now(timezone.utc) - timedelta(minutes=30),
+        stopped=False)
+    session.add(access_session)
+    await session.commit()
+    yield access_session
+    if TEARDOWN:
+        await session.execute(delete(AccessSession)
+                              .where(AccessSession.id == access_session.id))
         await session.commit()
