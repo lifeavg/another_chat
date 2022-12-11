e330ea45e6af1ee7c55162556b29b1d4d626074b
Artem <44943333+lifeavg@users.noreply.github.com>
2022-12-11 19:27:11 +0300

    format

--- auth/test/test_security.py
+++ auth/test/test_security.py
@@ -1,22 +1,28 @@
+from datetime import datetime, timedelta, timezone
+
 import pytest
 from pytest_mock import mocker
-from datetime import datetime, timezone, timedelta
-from auth.security import password_hash, verify_password, create_token, verify_token, SEC_SECRET_SESSION, login_limit
+
 from auth.api.schemas import SessionTokenData
-from auth.db.query import AsyncSession
 from auth.db.models import LoginAttempt
-import asyncio
+from auth.db.query import AsyncSession
+from auth.security import (SEC_SECRET_SESSION, create_token, login_limit,
+                           password_hash, verify_password, verify_token)
+
 
 def test_password_hash():
     password = 'qwerty1234_'
     assert verify_password(password, password_hash(password)) is True
 
+
 def test_token():
     token = SessionTokenData(
         jti=1, sub=1,
         exp=datetime.now(timezone.utc).replace(microsecond=0))
-    verified_token = verify_token(create_token(token.dict()), SessionTokenData, SEC_SECRET_SESSION)
+    verified_token = verify_token(create_token(
+        token.dict()), SessionTokenData, SEC_SECRET_SESSION)
     assert token == verified_token
+
 
 @pytest.mark.asyncio
 async def test_login_limit_not_reached(mocker):
@@ -24,6 +30,7 @@ async def test_login_limit_not_reached(mocker):
         'auth.db.query.login_attempt_by_fingerprint',
         return_value=[])
     assert (await login_limit(AsyncSession(), 'qwer', 30, 5)) is None
+
 
 @pytest.mark.asyncio
 async def test_login_limit_reached(mocker):
