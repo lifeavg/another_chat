e57f910525a792a068eb16fc4b3e6a72bd1ac8e8
Artem <44943333+lifeavg@users.noreply.github.com>
2022-12-26 22:32:05 +0300

    db object method call test

--- auth/test/test_api_users.py
+++ auth/test/test_api_users.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta, timezone
+from unittest.mock import MagicMock
 
 import pytest
 from httpx import AsyncClient
@@ -13,9 +14,9 @@ BASE_URL: str = 'http://localhost:8080/users'
 
 
 @pytest.fixture
-def authorization(permissions: list[str]) -> dict[str, str]:
+def authorization(perms: list[str]) -> dict[str, str]:
     token_exp = datetime.now(timezone.utc) + timedelta(days=2)
-    access_data = AccessTokenData(jti=1, sub=1, pms=permissions, exp=token_exp)
+    access_data = AccessTokenData(jti=1, sub=1, pms=perms, exp=token_exp)
     return {'Authorization': 'Bearer ' +
             create_token(access_data.dict(), SEC_SECRET_ACCESS)}
 
@@ -28,7 +29,9 @@ def user():
 
 @pytest.fixture
 def user_with_permissions():
-    return User(id=1, login='a', password='b', confirmed=True, active=True,
+    # def append(*arg):
+    #     pass
+    user = User(id=1, login='a', password='b', confirmed=True, active=True,
                 created_timestamp=datetime(
                     2020, 11, 18, 11, 12, 13, 120, timezone.utc),
                 permissions=[
@@ -37,10 +40,18 @@ def user_with_permissions():
                     Permission(id=6, name='p2', service_id=3,
                                expiration_min=10)
                 ])
+    # user.permissions.append = append.__get__(user.permissions)
+    return user
+
+
+@pytest.fixture
+def permissions():
+    return [Permission(id=7, name='p7', service_id=3, expiration_min=10),
+            Permission(id=8, name='p8', service_id=5, expiration_min=10)]
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize('permissions', [['auth_inf_user'],])
+@pytest.mark.parametrize('perms', [['auth_inf_user'],])
 async def test_user_data_ok(mocker, authorization, user):
     mocker.patch(
         'auth.db.query.user_by_id',
@@ -54,7 +65,7 @@ async def test_user_data_ok(mocker, authorization, user):
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize('permissions', [['auth_inf_user'],])
+@pytest.mark.parametrize('perms', [['auth_inf_user'],])
 async def test_user_data_not_found(mocker, authorization):
     mocker.patch(
         'auth.db.query.user_by_id',
@@ -67,7 +78,7 @@ async def test_user_data_not_found(mocker, authorization):
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize('permissions', [['auth'],])
+@pytest.mark.parametrize('perms', [['auth'],])
 async def test_user_data_no_permission(authorization):
     async with AsyncClient(
             app=app, base_url=BASE_URL, headers=authorization) as ac:
@@ -77,7 +88,7 @@ async def test_user_data_no_permission(authorization):
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize('permissions', [['auth_adm'],])
+@pytest.mark.parametrize('perms', [['auth_adm'],])
 async def test_user_permissions_ok(mocker, user_with_permissions, authorization):
     mocker.patch(
         'auth.db.query.user_with_permissions',
@@ -91,7 +102,7 @@ async def test_user_permissions_ok(mocker, user_with_permissions, authorization)
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize('permissions', [['auth_adm'],])
+@pytest.mark.parametrize('perms', [['auth_adm'],])
 async def test_user_permissions_not_found(mocker, authorization):
     mocker.patch(
         'auth.db.query.user_with_permissions',
@@ -104,7 +115,7 @@ async def test_user_permissions_not_found(mocker, authorization):
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize('permissions', [['auth'],])
+@pytest.mark.parametrize('perms', [['auth'],])
 async def test_user_permissions_no_permission(mocker, authorization):
     mocker.patch(
         'auth.db.query.user_with_permissions',
@@ -114,3 +125,20 @@ async def test_user_permissions_no_permission(mocker, authorization):
         response = await ac.get('/1/permissions')
     assert response.status_code == 403
     assert response.json() == {'detail': 'Invalid authorization code: type'}
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize('perms', [['auth_adm'],])
+async def test_add_user_permissions_ok(mocker, user_with_permissions, permissions, authorization):
+    mocker.patch(
+        'auth.db.query.user_with_permissions',
+        return_value=user_with_permissions)
+    mocker.patch(
+        'auth.db.query.permissions_by_names',
+        return_value=permissions)
+    user_with_permissions.permissions.append = MagicMock()
+    async with AsyncClient(
+            app=app, base_url=BASE_URL, headers=authorization,) as ac:
+        response = await ac.post('/1/permissions/add', json=[p.name for p in permissions])
+    assert response.status_code == 200
+    user_with_permissions.permissions.append.assert_called_with(*permissions)
+
